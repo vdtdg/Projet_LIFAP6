8d80148cc3ce76d3c30258b2f46b164f39498e53
vdtdg <v.dethezan@gmail.com>
2017-02-08 17:27:21 +0100

    Add struct Pioche

--- pioche.hpp
+++ pioche.hpp
@@ -3,27 +3,29 @@
 
 //structure de données
 struct Pioche {
-  //... votre code ici
+  std::vector <char> tas;
+  std::vector <char> defausse;
+  int taille_elt;
 } ;
 
 //initialisation de la pioche.
 //  - taille_elt est le nombre d'octets necessaires pour stocker une carte
-void pioche_init(Pioche& pioche, int taille_elt) ;
+void pioche_init(Pioche& pioche, int taille_elt);
 
 //pioche d'une carte
 //  - cible est l'adresse d'une zone memoire ou la carte piochee est inscrite
-void pioche_pioche(Pioche& pioche, void* cible) ;
+void pioche_pioche(Pioche& pioche, void* cible);
 
 //defausse d'une carte
 //  - carte est l'adresse d'une zone memoire ou la carte defaussee est inscrite
-void pioche_defausse(Pioche& pioche, const void* carte) ;
+void pioche_defausse(Pioche& pioche, const void* carte);
 
 //melange de la pioche. Toutes les cartes (pioche et defausse) sont melangees
 //pour former une nouvelle pioche.
-void pioche_melange(Pioche& pioche) ;
+void pioche_melange(Pioche& pioche);
 
 //liberation de la memoire occupee par la pioche. Apres cette fonction il est
 //necessaire d'appeler a nouveau init pour que la pioche puisse etre utilisee.
-void pioche_suppr(Pioche& pioche) ;
+void pioche_suppr(Pioche& pioche);
 
 #endif
