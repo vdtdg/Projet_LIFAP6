4795f24ad27f52feeb486ea156d555b20f20e157
vdtdg <v.dethezan@gmail.com>
2017-04-02 20:09:12 +0200

    ébauche tour joueur_simple

--- joueur_simple.cpp
+++ joueur_simple.cpp
@@ -2,9 +2,12 @@
 
 void joueur_simple_init(JoueurSimple& joueur) {
   joueur.points = 0;
-  mdp = rand() % 2400 + 1 ;
-  indice = -1;
-  
+  joueur.mdp = rand() % 2400 + 1 ;
+  joueur.indice = -1;
+  // obj nul
+  joueur.obj.ville1 = 0;
+  joueur.obj.ville2 = 0;
+  joueur.obj.points = 0;
 }
 
 void joueur_simple_suppr(JoueurSimple& joueur) {
@@ -20,11 +23,70 @@ void joueur_simple_config_indice(JoueurSimple& joueur, int indice) {
 }
 
 void joueur_simple_tour(Jeu& jeu, JoueurSimple& joueur) {
+  liste_liaison mon_pcc; // mon plus court chemin
+  int couleur_w_l; // couleur de la prochaine liaison
+  int nombre_w_l; // longueur de la prochaine liaison
+  int nombre_w_p; // nombre de wagons de cette couleur que je possède.
+
+  int nb_carte_piochee = 0;
   // DEBUT du tour.
-  // Si j'ai pas d'objectif, j'en pioche un, et j'applique dijkstra dessus -> chemin (liste de liaison)
-  // Je vérifie que les liaisons de chemin n'ai pas de propriétaire, sinon j'applique Dijkstra.
+  // Si j'ai pas d'objectif :
+  //     J'en pioche un
+  //     Je le stocke dans ma structure
+  if(joueur.obj.ville1 == 0 && joueur.obj.ville2 == 0) {
+    pioche_pioche(jeu.objectifs, joueur.obj);
+  }
+  
+  // J'applique dijkstra sur l'objectif -> chemin (liste de liaison)
+  mon_pcc = plus_court_chemin(jeu, joueur.obj);
+  
   // Je regarde le nombre de carte wagon nécessite la prochaine liaison
-  // Si j'ai assez de cartes, je la prend, et je l'enlève de ma liste
-  // Sinon je regarde dans les cartes découvertes si elles correspondent aux cartes que je veux, je prends un maximum de deux. Je pioche 2-nb de carte déjà prises.
+  couleur_w = jeu.liaisons[mon_pcc[0]].couleur;
+  nombre_w = jeu.liaisons[mon_pcc[0]].longueur;
+  for(int i = 0; i < joueur.wagons.size(); i++) {
+    if(joueur.wagons[i].couleur == couleur_w) {
+      nombre_w_p++;
+    }
+  }
+  
+  // Si j'ai assez de cartes :
+  if(nombre_w - nombre_w_p <= 0) {
+  //     Je prend la liaison
+    void jeu_prendre_liaison(Jeu& jeu, int ville1, int ville2, int joueur, int mdp) ;
+    jeu_prendre_liaison(jeu, jeu.liaisons[mon_pcc[0]].ville1, jeu.liaisons[mon_pcc[0]].ville2, joueur.indice, joueur.mdp);
+  //     Je l'enlève de ma liste des liaisons à atteindre en changeant l'objectif !
+  //     La ville de départ de l'objectif devient celle atteinte dernièrement.
+    obj.ville1 = jeu.liaisons[mon_pcc[0]].ville2;
+  }
+  // Sinon
+  else {
+  //     Je parcours les cartes de la pioche découverte
+    for(int i = 0; i < 4; i++) {
+  //     Si la carte correspond à la carte que je veux
+      if(jeu.carte_visibles[i] == couleur_w) {
+  //          Je la prend
+	jeu_pioche_visible(jeu, i, joueur.indice, joueur.mdp);
+	nb_carte_piochee ++;
+  //          Si nb_carte_piochee == 2 :
+  //             FIN du tour.
+	if(nb_carte_piochee ==2) {
+	  break;
+	}
+      }
+    }
+  //     Je pioche 2-nb_carte_piochee déjà prises.
+    for(int i = 0 ; i < 2 - nb_carte_piochee ; i++) {
+      joueur.wagons.resize(joueur.wagons.size() + sizeof(Carte));
+      pioche_pioche(jeu.cartes, joueur.wagons[joueur.wagons.size() - sizeof(Carte)]);
+    }
+  
   // FIN du tour.
+}
+
+liste_liaison plus_court_chemin(Jeu& jeu, const Objectif &obj) {
+  liste_liaison liste_plus_court_chemin;
+  
+
+
+  return liste_plus_court_chemin;
 }
--- joueur_simple.hpp
+++ joueur_simple.hpp
@@ -3,6 +3,8 @@
 
 #include "jeu.hpp"
 
+typedef vector<int> liste_liaison;
+
 struct JoueurSimple {
   std::vector <carte> wagons;
   Objectif obj;
@@ -12,24 +14,28 @@ struct JoueurSimple {
   
 } ;
 
-//initialisation
+// initialisation
 void joueur_simple_init(JoueurSimple& joueur) ;
 
-//suppression
+// suppression
 void joueur_simple_suppr(JoueurSimple& joueur) ;
 
-//acces au mot de passe a fournir au jeu. Le mot de passe sert a s'assurer que
-//le joueur ne tente pas de se faire passer pour un autre. Il sera utilise par
-//le joueur pour toutes les actions sur le jeu qui verifient l'identite du
-//joueur.
+// acces au mot de passe a fournir au jeu. Le mot de passe sert a s'assurer que
+// le joueur ne tente pas de se faire passer pour un autre. Il sera utilise par
+// le joueur pour toutes les actions sur le jeu qui verifient l'identite du
+// joueur.
 int joueur_simple_mdp(JoueurSimple& joueur) ;
 
-//prise en compte de l'indice du joueur fourni par le jeu. Le joueur pourra
-//utiliser cet indice pour s'identifier aupres du jeu pour les actions qui le
-//demandent : piocher, defausser, prendre des liaisons, ...
+// prise en compte de l'indice du joueur fourni par le jeu. Le joueur pourra
+// utiliser cet indice pour s'identifier aupres du jeu pour les actions qui le
+// demandent : piocher, defausser, prendre des liaisons, ...
 void joueur_simple_config_indice(JoueurSimple& joueur, int indice) ;
 
-//un tour de jeu pour le joueur simple
+// un tour de jeu pour le joueur simple
 void joueur_simple_tour(Jeu& jeu, JoueurSimple& joueur) ;
+
+// retourne une liste d'index de liaison representant le chemin le plus court
+// entre les points de l'objectif passé en paramètre.
+liste_liaison plus_court_chemin(Jeu& jeu, const Objectif &obj);
 
 #endif
